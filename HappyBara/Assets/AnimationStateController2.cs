5a662a76752a0a48199ec8da76c1829be84c2630
David Marcano <dfmr96@gmail.com>
2022-11-21 21:18:47 -0300

    Player with Animal walking animation added

--- HappyBara/Assets/AnimationStateController2.cs
+++ HappyBara/Assets/AnimationStateController2.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class AnimationStateController2 : MonoBehaviour
 {
+    Animator animator;
+    int isWalkingHash;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+
+        }
     }
 }
